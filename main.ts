d53a9ce98fb192744e51d3b42ab95b613a8a1dad
James Couch <timothy.james.couch@gmail.com>
2025-08-12 00:53:43 -0600

    changes

--- main.ts
+++ main.ts
@@ -19,7 +19,7 @@ async function app() {
 
   const alice = new AgentModel("alice", "gpt-oss:20b");
   const carol = new AgentModel("carol", "gpt-oss:20b");
-  const bob   = new AgentModel("bob", "gpt-oss:120b");
+  const bob   = new AgentModel("bob", "gpt-oss:20b");
   room.addModel(alice);
   room.addModel(carol);
   room.addModel(bob);
