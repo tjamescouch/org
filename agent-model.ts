cc19f9acc6e526e0423f8a7920a452d1f4b2eeca
James Couch <timothy.james.couch@gmail.com>
2025-08-13 02:59:02 -0600

    deploying to VM

--- agent-model.ts
+++ agent-model.ts
@@ -8,6 +8,18 @@ export const Reset = () => "\x1b[0m";
 export const CyanTag = () => "\x1b[36m";
 export const YellowTag = () => "\x1b[33m";
 export const RedTag = () => "\x1b[31m";
+export const GreenTag = () => "\x1b[32m";
+export const BlueTag = () => "\x1b[34m";
+export const MagentaTag = () => "\x1b[35m";
+export const WhiteTag = () => "\x1b[37m";
+export const BrightBlackTag = () => "\x1b[90m"; // gray
+export const BrightRedTag = () => "\x1b[91m";
+export const BrightGreenTag = () => "\x1b[92m";
+export const BrightYellowTag = () => "\x1b[93m";
+export const BrightBlueTag = () => "\x1b[94m";
+export const BrightMagentaTag = () => "\x1b[95m";
+export const BrightCyanTag = () => "\x1b[96m";
+export const BrightWhiteTag = () => "\x1b[97m";
 const withTimeout = <T>(p: Promise<T>, ms: number, label = "timeout"): Promise<T> =>
   Promise.race([
     p,
@@ -767,7 +779,7 @@ Be concise.
 ok: ${result.ok}
 exit_code: ${result.exit_code}
 --- stdout ---
-${CyanTag()}${sOut}${Reset()}
+${MagentaTag()}${sOut}${Reset()}
 --- stderr ---
 ${RedTag()}${sErr}${Reset()}
 `);
