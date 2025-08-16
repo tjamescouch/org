ae5c7c0961659b5157596e61a357fd55036e18cd
James Couch <timothy.james.couch@gmail.com>
2025-08-16 16:29:06 -0600

    boom

--- src/runtime-fixes/bootstrap.ts
+++
@@ -1,4 +0,0 @@
-import "./muted-colors";
-import "./think-flatten";
-import { installDebugHooks } from "../core/debug-hooks";
-installDebugHooks().catch(e => console.warn("debug-hooks install failed:", e));
--- test/think-flatten.test.ts
+++
@@ -1,8 +0,0 @@
-import { flattenThink } from "../src/core/utils/think";
-
-test("flattenThink collapses multi-line CoT block (given example)", () => {
-  const input = [
-    "We","need","to","respond","with","greeting","and","follow","-up",".","Simple","."
-  ].join("\n");
-  expect(flattenThink(input)).toBe("We need to respond with greeting and follow -up . Simple .");
-});
